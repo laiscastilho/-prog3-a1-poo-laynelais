5dbaaeeeb22399cfda96a1e8a2265a990d210bb6
laiscastilho <143663366+laiscastilho@users.noreply.github.com>
2025-04-15 21:00:57 -0300

    Correção dos links do index

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ require_once 'classes/Sessao.php';
 $sessao = new Sessao();
 
 echo "<h1>Bem-vindo(a)!</h1>";
-echo "<a href='login.php'>Login</a> | <a href='cadastro.php'>Cadastro</a>";
+echo "<a href='/login.php'>Login</a> | <a href='/cadastro.php'>Cadastro</a>";
 
 ?>
